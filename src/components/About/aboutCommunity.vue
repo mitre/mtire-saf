a4c890fc6a2da2545883ecd8e47735620c93017f
Lipee Vora <lvora@mitre.org>
2019-12-03 14:44:06 -0500

    Removed all parts of the page non-MITRE related, and began adding MITRE info

--- src/components/About/aboutCommunity.vue
+++ src/components/About/aboutCommunity.vue
@@ -2,25 +2,25 @@
   <v-container fluid class="py-0 my-0 mt-2 mb-5">
     <v-row class="py-0 my-0" align="center">
       <v-col md="4" sm="4" cols="12" class="py-0 my-0">
-        <v-img
+        <!-- <v-img
           :src="require('@/assets/img/do-not-remove/about.png')"
           :lazy-src="require('@/assets/img/do-not-remove/about.png')"
           class="mb-3"
           width="100%"
-        >
+        > -->
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
               <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
             </v-row>
           </template>
-        </v-img>
+        <!-- </v-img> -->
       </v-col>
       <v-col md="8" sm="8" cols="12" class="py-0 my-0">
         <p class="google-font mt-0" style="font-size:110%">{{communitydata.CommunityLongDec}}</p>
         <p
           class="google-font mt-0 mb-0"
           style="font-size:110%"
-        >*insert text from slide deck</p>
+        >SAF focuses on building security into DevOps with the goal of preventing vulnerable applications from reaching production. </p>
         <br />
         <!-- <v-btn
           :href="communitydata.CommunityMeetupLink"
