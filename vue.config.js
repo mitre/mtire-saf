e332e1107028ee599d8a3ba494288cb05aa56e66
Amndeep Singh Mann <amann@mitre.org>
2020-06-11 19:50:59 -0400

    Added dashify to transpiledependencies

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
   },
   productionSourceMap: false,
-  transpileDependencies: ["vuetify"],
+  transpileDependencies: ["vuetify, dashify"],
   //  publicPath: process.env.NODE_ENV === 'production'
   //    ? '/mitre-saf/'
   //    : '/'
