f7e1699c75a40e00b2133a1e647ab1d5014b5ef6
Amndeep Singh Mann <amann@mitre.org>
2020-06-19 03:15:27 -0400

    Fixed the profiletemplatecompact, and the validation page.  ie works in a non-breaking manner now though the behavior doesn't line up exactly with modern browsers

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -1,29 +1,32 @@
 <template>
-  <v-content class="pa-0">
-    <v-container fluid class="pa-0">
-      <v-row justify="center" align="center" class="pa-2">
-        <v-col md="12" lg="10" xs="12" class="pa-0 ma-0">
-          <Header>
-            <h3 slot="title">Validation</h3>
-          </Header>
-          <div class="mx-4 google-font">
-            <p class="ma-0">
-              These open-source community-based InSpec profiles validate the security of
-              common system components. MITRE is helping to provide stewardship over
-              these profiles, hosted here and at other community vendor sites. If you
-              are interested in new profiles, please contact us at
-              <a
-                :href="mail_link"
-              >{{ db.communityEmail }}</a>. If you are interested in developing and contributing your own
-              profiles, please see our
-              <router-link to="training">training material</router-link>.
-            </p>
-          </div>
-        </v-col>
-      </v-row>
-      <profileTemplateCompact :profiles="baselines.baselines" :categoryOrder="categoryOrder" class="mx-1"/>
-    </v-container>
-  </v-content>
+  <v-container fluid class="pa-0">
+    <v-row justify="center" align="center" class="pa-2">
+      <v-col cols="12" lg="10" class="pa-0 ma-0">
+        <Header>
+          <h3 slot="title">Validation</h3>
+          <p slot="subtitle" class="ma-0 google-font">
+            These open-source community-based InSpec profiles validate the security of
+            common system components. MITRE is helping to provide stewardship over
+            these profiles, hosted here and at other community vendor sites. If you
+            are interested in new profiles, please contact us at
+            <a
+              :href="mail_link"
+            >
+              {{ db.communityEmail }}
+            </a>
+            . If you are interested in developing and contributing your own
+            profiles, please see our
+            <router-link to="training">training material</router-link>.
+          </p>
+        </Header>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" lg="10">
+        <profileTemplateCompact :profiles="baselines.baselines" :categoryOrder="categoryOrder"/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
