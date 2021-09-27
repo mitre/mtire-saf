140383d6ee7d279a864ade9417f267b4cb9f1234
rbclark <rbclark@users.noreply.github.com>
2021-09-27 00:31:15 +0000

    deploy: 2a3acbab663244461bf40a9f7fd65cccd7f11405

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.efcd3cc856d06cc44478ecff39af2c85.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.019c573717de62d8b399d5e5a97edad4.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener("message", e => {
   if (!e.data) {
