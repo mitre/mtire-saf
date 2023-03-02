77b262d00cbc1fc82298f799d778efeb709641d9
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2023-03-02 13:05:06 -0600

    remove latest training RSVP
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- src/components/training/RSVPBlock.vue
+++ src/components/training/RSVPBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="mb-2">Upcoming Class Offerings: SAF Training February 2023!</h2>
+    <!-- <h2 class="mb-2">Upcoming Class Offerings: SAF Training February 2023!</h2>
     <p >
       Sponsored by <a href="https://www.mda.mil/" target="_blank">MDA</a>, the MITRE SAF Team is happy to announce its first round of security automation training courses for 2023. 
       These are free, on-line virtual, hands-on, instructor-led two-day courses.
@@ -17,8 +17,8 @@
       >
       will again be offered in Febraury 22-23, 2023 (10am-5pm ET both days) (12 CPE).</li>
     </ul>
-    <p >
-      <v-btn
+    <p > -->
+      <!-- <v-btn
         elevation="8"
         large
         outlined
@@ -33,7 +33,7 @@
       Please RSVP if you are interested in attending. If you are having trouble with the form, please email
       <a :href="'mailto:' + communityData.trainingContactEmail" target="_blank"
         >SAF@groups.mitre.org.</a
-      >
+      > -->
       <br>
       For information on prerequisites and resources for learning Ruby, look at the saf-training material available 
       <a
@@ -46,7 +46,7 @@
         target="_blank"
         >here.</a
       >
-    </p>
+    <!-- </p> -->
   </div>
 </template>
 <script>
