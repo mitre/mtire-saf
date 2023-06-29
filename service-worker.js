c6badf0c005e193ab63d7760eaeb06724703696c
wdower <wdower@users.noreply.github.com>
2023-06-29 15:16:00 +0000

    deploy: 3c8dfcc8c0ab9d51406b5e04e7e934add57d15aa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.d829883094088549866cfdd5819f0db0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.6cc6df8644e6dc57ee602a5e44b20d1e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener("message", e => {
   if (!e.data) {
