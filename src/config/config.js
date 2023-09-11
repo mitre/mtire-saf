f19ba71053138786523025d9f5d75d72218821a7
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-09-11 15:23:15 -0400

    Init V2
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- src/config/config.js
+++
@@ -1,6 +0,0 @@
-export const configData = {
-  MeetupURLName: "MITRE-Tech-Talks", // Example GDG-Jalandhar  CHAPTER_URL_NAME
-  MediumBlogPublicationUsername: "mitre-tech-talks"
-};
-
-// For urlname: extract the urlname from meetup chapter link-> https://meetup.com/GDG-Jalandhar
